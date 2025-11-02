47bb5c10c66dc89d3d4fe8f6eacce2647f3fff53
ZilhaKalac <zilhakalac@gmail.com>
2025-11-03 01:51:51 +0300

    RegistrationPage created, registration feature created, user class changed, registration steps class created

---
+++ src/test/java/US_601/RegistrationSteps.java
@@ -0,0 +1,31 @@
+package US_601;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class RegistrationSteps {
+
+    @When("Navigate to the webpage")
+    public void navigate_to_the_webpage() {
+        System.out.println("Web page is navigated");
+    }
+
+    @Then("Click the Register Link on the home page")
+    public void clickTheRegisterLinkOnTheHomePage() {
+
+    }
+
+    @And("Fill the additional details")
+    public void fillTheAdditionalDetails() {
+    }
+
+    @Then("Click Register button")
+    public void clickRegisterButton() {
+    }
+
+    @Then("Verify Registration is successful")
+    public void verifyRegistrationIsSuccessful() {
+    }
+}
+
